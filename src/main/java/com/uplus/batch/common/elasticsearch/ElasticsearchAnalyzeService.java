d1d7040eba464187c0f6d1086f48fe99bcc28410
Ujin28 <melon4092@naver.com>
2026-03-04 11:44:09 +0900

    feat: keyword rank batch 작업 중간 코드(작업중)

---
+++ src/main/java/com/uplus/batch/common/elasticsearch/ElasticsearchAnalyzeService.java
@@ -0,0 +1,34 @@
+package com.uplus.batch.common.elasticsearch;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.indices.AnalyzeRequest;
+import co.elastic.clients.elasticsearch.indices.AnalyzeResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ElasticsearchAnalyzeService {
+
+  private final ElasticsearchClient elasticsearchClient;
+
+  public List<String> analyze(String text) throws IOException {
+
+    AnalyzeResponse response = elasticsearchClient.indices().analyze(
+        AnalyzeRequest.of(a -> a
+            .analyzer("nori")
+            .text(text)
+        )
+    );
+
+    return response.tokens()
+        .stream()
+        .map(t -> t.token())
+        .filter(token -> token.length() > 1)
+        .collect(Collectors.toList());
+  }
+}
